7dde62909fca0cd0b11c9d50ee63203cfcb94ce8
yudhaime <36374524+yudhaime@users.noreply.github.com>
2019-09-08 08:53:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ client.on('ready', () => {
 
 client.on('message', message => {
   if (message.author.bot) return;
-  // This is where we'll put our code.
-  if (message.content.indexOf(config.prefix) !== 0) return;
  
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
