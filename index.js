91cc841914c4ec56cdea5a4710b4ff6505b8a565
yudhaime <36374524+yudhaime@users.noreply.github.com>
2019-09-08 08:24:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix ='!';
+const prefix ='%';
 const args = message.content.slice(prefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
 
