4502ea9c75a63f661411dca3d9fceee603843bc1
yudhaime <36374524+yudhaime@users.noreply.github.com>
2019-09-08 08:47:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix ='%';
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
 
 client.on('ready', () => {
     console.log('Siap Bro!!!');
 });
 
 client.on('message', message => {
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
     if(command === 'ping') {
         message.channel.send('Pong!');
     } else
