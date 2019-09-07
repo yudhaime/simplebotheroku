42a92f7b93b9f0e8c07c5b5bff58658bab4596b3
yudhaime <36374524+yudhaime@users.noreply.github.com>
2019-09-08 07:55:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+    console.log('I am ready!');
 });
 
-client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
-    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+client.on('message', message => {
+    if (message.content === 'ping') {
+       message.reply('pong');
+       }
 });
-
-client.login(process.env.TOKEN);
+client.login(process.env.BOT_TOKEN)
