171bb306f65e2cae6f309f56be542cd6c9f9d2a0
yudhaime <36374524+yudhaime@users.noreply.github.com>
2019-09-08 08:55:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.author.bot) return;
  
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
     if(command === 'ping') {
         message.channel.send('Pong!');
