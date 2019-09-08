e50cb5c34096df2d81ce2b3e98e671ce5d7682f9
yudhaime <36374524+yudhaime@users.noreply.github.com>
2019-09-08 09:40:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,20 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.author.bot) return;
  
-  const args = message.content.slice(prefix.length).trim().split('%');
+  const args = message.content.slice(prefix.length).split('%');
   const command = args.shift().toLowerCase();
+  const umumkan = client.channels.get("id", "620055202438840330")
     if(command === 'ping') {
         message.channel.send('Pong!');
     } else
     if (command === 'blah') {
         message.channel.send('Meh.');
     } else
-    if (command === "asl") {
+    if (command === "asl ") {
         let age = args[0]; // Remember arrays are 0-based!.
         let sex = args[1];
         let location = args[2];
+    umumkan.sendMessage(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
     message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
 }
 });
